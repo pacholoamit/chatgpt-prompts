798ef2861638207137a0e0484c9ac9efe3c6ba91
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-06 15:51:04 +0800

    Add Scrape script

---
+++ scripts/scrape.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import * as csv from "csvtojson";
+import * as fs from "fs";
+import { camelCase } from "lodash";
+
+interface CSVPrompts {
+  act: string;
+  prompt: string;
+}
+
+const cleanBackTicks = (str: String) => str.replace(/`/g, "\\`");
+
+const createTemplate = ({ act, prompt }: CSVPrompts) => {
+  const template = `
+export const ${camelCase(act)} = (instance: ChatGPTAPI) => {
+  const prompt = \`${cleanBackTicks(prompt)}\`;
+  return {
+    /**
+     * @description ${prompt}
+     * @param {string} message
+     * @returns {Promise<ChatMessage>} ChatGPT Message
+     */    
+    ${camelCase(act)}: async (message: string): Promise<ChatMessage> => createPromptFactory(instance, prompt)(message),
+  };
+};
+`;
+
+  return template;
+};
+
+const main = async () => {
+  const res = await axios.get("https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv");
+
+  const prompts = await csv().fromString(res.data);
+
+  prompts.forEach((prompt) => {
+    fs.appendFileSync("prompts.ts", createTemplate(prompt));
+  });
+};
+
+main().catch((err) => console.log(err));
