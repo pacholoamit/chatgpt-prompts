4d7afef05b0040e29a815edd08503766619048b2
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-06 16:09:56 +0800

    Import prompts

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -10,8 +10,8 @@ interface CSVPrompts {
 
 const cleanBackTicks = (str: String) => str.replace(/`/g, "\\`");
 
-const createTemplate = ({ act, prompt }: CSVPrompts) => {
-  const template = `
+const createFunctionTemplate = ({ act, prompt }: CSVPrompts) => {
+  return `
 export const ${camelCase(act)} = (instance: ChatGPTAPI) => {
   const prompt = \`${cleanBackTicks(prompt)}\`;
   return {
@@ -24,17 +24,19 @@ export const ${camelCase(act)} = (instance: ChatGPTAPI) => {
   };
 };
 `;
-
-  return template;
 };
 
+const createImportTemplate = ({ act }: CSVPrompts) => {
+  return `...prompts.${camelCase(act)}(instance),`;
+};
 const main = async () => {
   const res = await axios.get("https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv");
 
   const prompts = await csv().fromString(res.data);
 
   prompts.forEach((prompt) => {
-    fs.appendFileSync("prompts.txt", createTemplate(prompt));
+    fs.appendFileSync("prompts.txt", createFunctionTemplate(prompt));
+    fs.appendFileSync("imports.txt", createImportTemplate(prompt));
   });
 };
 
