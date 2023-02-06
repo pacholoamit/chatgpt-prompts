164761122aa09ce503b933643fdee96c299f7431
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-06 16:36:25 +0800

    create examples folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as chatgpt from 'chatgpt';
-import { ChatGPTAPI } from 'chatgpt';
-
-declare const createChatGPTPrompt: (instance: ChatGPTAPI) => {
-    linuxTerminal: (message: string) => Promise<chatgpt.ChatMessage>;
-};
-
-export { createChatGPTPrompt };
--- examples/basic.ts
+++ examples/basic.ts
@@ -1,15 +1,19 @@
-import { createChatGPTPrompt } from "../dist";
+import { createChatGPTPrompt } from "../src";
 import { ChatGPTAPI } from "chatgpt";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 (async () => {
   const instance = new ChatGPTAPI({
-    apiKey: "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",
+    apiKey: process.env.OPEN_AI_API_KEY,
   });
 
   const prompt = createChatGPTPrompt(instance);
 
   let res = await prompt.linuxTerminal("pwd");
   console.log(res.text);
+
   res = await prompt.linuxTerminal("echo hello world");
   console.log(res.text);
 })();
