bb29896f923c8241cfbe18b777994f0b45e34e78
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-07 12:57:48 +0800

    generate test file

--- generators/prompts/index.ts
+++ generators/prompts/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world");
+import ts from "typescript";
+import fs from "fs";
+
+const name = "test.ts";
+const code = "console.log('hello world')";
+const sourceFile = ts.createSourceFile(name, code, ts.ScriptTarget.Latest, false, ts.ScriptKind.TS);
+
+fs.writeFileSync(name, sourceFile.getFullText());
