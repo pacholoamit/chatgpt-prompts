c2e24bbff4d876d17d963bf75e88eb1e81871aa1
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-19 03:06:50 +0800

    Generate readme & modify example to reflect v2.0.0

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,12 +1,9 @@
-import { ChatGPTAPI } from "chatgpt";
 import { createChatGPTPrompt } from "chatgpt-prompts";
 
 const run = async () => {
-  const instance = new ChatGPTAPI({
+  const prompt = createChatGPTPrompt({
     apiKey: "OPEN_AI_API_KEY",
   });
-
-  const prompt = createChatGPTPrompt(instance);
 
   let res = await prompt.accountant("Why am I still broke as a software engineer?");
   console.log(res.text);
