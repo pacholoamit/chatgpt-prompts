4b4563a6167293edea4c7e2d7a7c23f7805a69e2
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-19 00:54:55 +0800

    Create basic test

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  testEnvironment: "node",
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default jestConfig;
---
+++ test/main.test.ts
@@ -0,0 +1,10 @@
+import { createChatGPTPrompt } from "../src";
+import { describe, expect, test, it } from "@jest/globals";
+
+describe("pass", () => {
+  it("should pass", () => {
+    const bool = true;
+
+    expect(bool).toBe(false);
+  });
+});
