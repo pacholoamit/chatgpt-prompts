f3968ad445e67939304c7312ee2d16d5d55e2221
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-19 03:20:15 +0800

    Cleanup generators

--- generators/src/index.ts
+++ generators/src/index.ts
@@ -1,24 +1,24 @@
-import ejs from "ejs";
-import * as fs from "fs";
-import { funcTemplate, methodImportTemplate, typeTemplate } from "./template";
 import { promptsFile, methodImportsFile, templateMarkdownFile, readmeFile, typesFile } from "./constants";
 import { PromptCsvField } from "./types";
 import createReadmeGenerator from "./readme-generator";
 import createPromptsGenerator from "./prompts-generator";
 
-const generateReadme = async (prompts: PromptCsvField[]) => {
+const generateReadme = async (prompts: PromptCsvField[]): Promise<void> => {
   const readmeGenerator = createReadmeGenerator(templateMarkdownFile, readmeFile);
   const readmePrompts = readmeGenerator.format(prompts);
   readmeGenerator.generate(readmePrompts);
 };
 
-const generateCode = async (instance: ReturnType<typeof createPromptsGenerator>, prompts: PromptCsvField[]) => {
+const generateCode = async (
+  instance: ReturnType<typeof createPromptsGenerator>,
+  prompts: PromptCsvField[]
+): Promise<void> => {
   instance.writeInterface(typesFile, prompts);
   instance.writePromptsFunctions(promptsFile, prompts);
   instance.writeMainImports(methodImportsFile, prompts);
 };
 
-const main = async () => {
+const main = async (): Promise<void> => {
   const promptsGenerator = createPromptsGenerator();
   const prompts = await promptsGenerator.getPrompts();
 
