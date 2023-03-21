95e14bdd23aabc0147183dcb8199ec91e93f92f3
pacholoamit <pacholoamit.tech@gmail.com>
2023-03-21 16:27:18 +0800

    Remove conversationId

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,24 +1,20 @@
 import { ChatGPTAPI, ChatMessage } from "chatgpt";
 
-let conversationId: string | undefined;
 let parentMessageId: string | undefined;
 
 export const createPromptFactory = (instance: ChatGPTAPI, prompt: string) => {
   return async (message: string) => {
     let res: ChatMessage | undefined;
 
-    if (!conversationId || !parentMessageId) {
+    if (!parentMessageId) {
       res = await instance.sendMessage(prompt);
-      conversationId = res.conversationId;
       parentMessageId = res.id;
     }
 
     res = await instance.sendMessage(message, {
-      conversationId,
       parentMessageId,
     });
 
-    conversationId = res.conversationId;
     parentMessageId = res.id;
 
     return res;
